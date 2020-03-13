4cc31e4fb31adb190c6f7ca5a46665dde1688171
Leon Bein <leon.bein@student.hpi.de>
2020-03-13 10:26:55 +0100

    Added visualization for (un-)deploy time (un-)subscription

--- app/lib/subscription-visualizer/SubscriptionVisualizer.js
+++ app/lib/subscription-visualizer/SubscriptionVisualizer.js
@@ -1,7 +1,8 @@
-import SubscriptionFinder from "./SubscriptionFinder";
+import SubscriptionFinder, { DEPLOYMENT_TIME, UNDEPLOYMENT_TIME } from "./SubscriptionFinder";
 import { isChoreography } from "./Utils";
 
 export default function SubscriptionVisualizer(viewer) {
+    this.viewer = viewer;
     this.subscriptionFinder = new SubscriptionFinder(viewer);
     this.overlays = viewer.get('overlays');
     this.elementRegistry = viewer.get('elementRegistry');
@@ -19,19 +20,21 @@ SubscriptionVisualizer.prototype.showFor = function(element) {
     const width = 5;
     let subscription = this.subscriptionFinder.findSubscriptionsFor(element);
 
-    let left = subscription.subscribeTasks.map(each => 
-        this.overlays.add(each, {
+    let left = subscription.subscribeTasks.map(each => {
+        if(each === DEPLOYMENT_TIME) each = this.processBounds();
+        return this.overlays.add(each, {
             position: {left: -width},
-            html: marker(width, element.height, '#00FF55AA')
+            html: marker(width, each.height, '#00FF55AA')
         })
-    );
+    });
 
-    let right = subscription.unsubscribeTasks.map(each =>
-        this.overlays.add(each, {
+    let right = subscription.unsubscribeTasks.map(each => {
+        if(each === UNDEPLOYMENT_TIME) each = this.processBounds();
+        return this.overlays.add(each, {
             position: {right: 0},
-            html: marker(width, element.height, '#FF2222AA')
+            html: marker(width, each.height, '#FF2222AA')
         })
-    );
+    });
     this.selectedElement = element;
     this.overlayIds = left.concat(right);
 }
@@ -50,6 +53,17 @@ SubscriptionVisualizer.prototype.selected = function(element) {
             this.showFor(element);
         }
     }
+}
+
+SubscriptionVisualizer.prototype.processBounds = function() {
+    const padding = 5;
+    let set = this.elementRegistry.getAll();
+    let x = Math.min(...set.map(each => each.x || Infinity)) - padding;
+    let y = Math.min(...set.map(each => each.y || Infinity));
+    let width = Math.max(...set.map(each => (each.x + each.width) || -Infinity)) - x + padding;
+    let height = Math.max(...set.map(each => (each.y + each.height) || -Infinity)) - y;
+    let res = {id : '__processBounds', x, y, width, height};
+    return res;
 }
 
 function marker(width, height, color) {
